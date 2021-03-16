71c9d7089e7c530f3e8139ba37fdf28f79856278
Edward Huang <ejhuang.2015@gmail.com>
2021-03-16 12:04:02 -0400

    Defined table columns

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,14 +6,26 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+    },
   },
   {
     sequelize,
-    timestamps: false,
+    timestamps: true,
     freezeTableName: true,
     underscored: true,
     modelName: 'product_tag',
+    paranoid: true,
   }
 );
 
